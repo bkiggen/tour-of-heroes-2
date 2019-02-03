c9035e3e8e26ed22acd0ef93ffbfe5c95faf2f7f
Ben Kiggen <benkiggen@gmail.com>
2019-02-03 09:54:58 -0800

    add broken code that is broken despite being a clone of the official docs, versioning problem?

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class MessageService {
+  constructor(){};
   messages: string[] = [];
 
   add(message: string) {
--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-message',
+  providers: [HeroService],
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
 
-  constructor(public messageService: MessageService) { }
+  constructor(public messageService: MessageService) {}
 
   ngOnInit() {
   }
